4d2e65d884458400fac673e855853fb230e9c3d7
Tran Hung Thinh <124481874+little-fox28@users.noreply.github.com>
2026-08-26 13:32:58 +0700

    feat: create reusable logo template component with support for customizer and theme settings

--- parts/components/common/logo.php
+++ parts/components/common/logo.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Logo Component Template Part
  * 
@@ -19,19 +20,41 @@ if (!defined('ABSPATH')) {
 // Get parameters
 $class = $args['class'] ?? '';
 $show_branding = $args['show_branding'] ?? true;
+
+// 1. Check Customizer logo
+$custom_logo_id = get_theme_mod('custom_logo');
+
+// 2. Check ACF Theme Settings logo (if configured)
+if (!$custom_logo_id && function_exists('wataco_get_theme_settings_field')) {
+    $custom_logo_id = wataco_get_theme_settings_field('global_logo');
+}
+
+$logo_url = '';
+$logo_alt = '';
+
+if ($custom_logo_id) {
+    $logo_url = is_numeric($custom_logo_id) ? wp_get_attachment_image_url((int) $custom_logo_id, 'full') : $custom_logo_id;
+    $logo_alt = is_numeric($custom_logo_id) ? get_post_meta((int) $custom_logo_id, '_wp_attachment_image_alt', true) : '';
+}
+
+if (empty($logo_url)) {
+    $logo_url = get_theme_file_uri('assets/images/wataco-logo.webp');
+}
+
+if (empty($logo_alt)) {
+    $logo_alt = get_bloginfo('name');
+}
 ?>
 
 <div class="shrink-0">
     <a href="<?php echo esc_url(home_url('/')); ?>" class="flex items-center space-x-3 hover:opacity-90 transition-opacity <?php echo esc_attr($class); ?>">
-        <?php
-        // Hardcoded SVG logo path
-        $svg_path = get_theme_file_uri('assets/images/wataco-logo-svg.svg');
-        echo '<img src="' . esc_url($svg_path) .  '" class="h-10 w-15 lg:h-12 lg:w-16 object-contain">';
-        ?>
-        
-        <div class="flex flex-col">
-            <span class="text-lg lg:text-xl font-black tracking-tighter leading-none text-white font-heading">WATACO</span>
-            <span class="text-[6px] lg:text-[8px] text-white font-bold tracking-[0.2em] uppercase mt-1">Member of Watanabe Create Group</span>
-        </div>
+        <img src="<?php echo esc_url($logo_url); ?>" alt="<?php echo esc_attr($logo_alt); ?>" class="h-10 w-15 lg:h-12 lg:w-16 object-contain">
+
+        <?php if ($show_branding) : ?>
+            <div class="flex flex-col">
+                <span class="text-lg lg:text-xl font-black tracking-tighter leading-none text-white font-heading">WATACO</span>
+                <span class="text-[6px] lg:text-[8px] text-white font-bold tracking-[0.2em] uppercase mt-1">Member of Watanabe Create Group</span>
+            </div>
+        <?php endif; ?>
     </a>
 </div>
